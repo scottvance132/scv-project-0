9290f9cca9d449b25fb85832ff0bce9753ebdf5f
scott <scottsvance7@gmail.com>
2022-06-22 11:45:35 -0400

    Add flask to app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from service.customer import Customer
+from flask import Flask, request
+
+app = Flask(__name__)
 
 c1 = Customer("Scott", "Vance", "07/12")
 print(c1.username)
 
+app.run(port=8080)
