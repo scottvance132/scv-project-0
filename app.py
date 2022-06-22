fff9fa3b038d5fc94f2fc01527788f39d6e4fcc7
scott <scottsvance7@gmail.com>
2022-06-22 14:03:43 -0400

    Update customer.py and app.py

--- app.py
+++ app.py
@@ -1,9 +1,51 @@
-from service.customer import Customer
+from service.customer import Customer, CustomerEncoder
 from flask import Flask, request
 
 app = Flask(__name__)
 
-c1 = Customer("Scott", "Vance", "07/12")
-print(c1.username)
+c1 = Customer("Scott", "Vance", "07/12", "checking", 1000.00)
+c2 = Customer("Mariia", "Vance", "07/29", "checking", 5.12)
+print(c1)
 
-app.run(port=8080)
+print(c1.get_first_name())
+print(c1.get_last_name())
+print(c1.get_birthday())
+print(c1.username)
+print(c1.get_account_info())
+
+# c1.set_last_name("Smith")
+# print(c1.get_last_name())
+# print(c1.get_username())
+
+# c1.set_birthday("12/12")
+# print(c1.get_username())
+
+customers = [
+    c1
+]
+
+
+# c1JSONData = json.dumps(c1, indent=4, cls=CustomerEncoder)
+
+
+@app.route("/test")
+def hello():
+    return "test executed successfully"
+
+
+@app.route("/customers")
+def get_all_customers():
+    my_customers = []
+    for key in customers:
+        customer = {
+            "username": key
+        }
+
+        my_customers.append(customer)
+
+    return{
+              "customers": my_customers
+    }, 200
+
+
+# app.run(port=8080)
--- service/customer.py
+++ service/customer.py
@@ -1,7 +1,48 @@
+from json import JSONEncoder
+
+
 class Customer:
-    def __init__(self, first_name, last_name, birthday):
+    def __init__(self, first_name, last_name, birthday, account_type, account_balance):
         self.__first_name = first_name
         self.__last_name = last_name
         self.__birthday = birthday.replace("/", '')
         self.username = f"{self.__first_name}{self.__last_name}{self.__birthday}"
+        self.__account = {"type": account_type, "balance": account_balance}
 
+    def __str__(self):
+        return f"Customer Object has the username: {self.username}"
+
+    def get_first_name(self):
+        return self.__first_name
+
+    def get_last_name(self):
+        return self.__last_name
+
+    def get_birthday(self):
+        return self.__birthday
+
+    def get_username(self):
+        return self.username
+
+    def get_account_info(self):
+        return self.__account
+
+    def set_first_name(self, first_name):
+        self.__first_name = first_name
+        self.username = f"{self.__first_name}{self.__last_name}{self.__birthday}"
+
+    def set_last_name(self, last_name):
+        self.__last_name = last_name
+        self.username = f"{self.__first_name}{self.__last_name}{self.__birthday}"
+
+    def set_birthday(self, birthday):
+        self.__birthday = birthday
+        self.username = f"{self.__first_name}{self.__last_name}{self.__birthday}"
+
+    def set_account_info(self, account_type, account_balance):
+        self.__account = {"type": account_type, "balance": account_balance}
+
+
+class CustomerEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
